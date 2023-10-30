3db327f02557b237a7f88aaa22c40f9f38320403
Alexis Suarez <a219201031@unison.mx>
2023-10-30 14:17:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+const a = 10;
+let b;
+b = 3.1416;
+function sayHello(msg) {
+    console.log(msg);
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+const a:number = 10;
+let b: String;
+
+b = 3.1416;
+
+function sayHello(msg){
+    console.log(msg)
+}
